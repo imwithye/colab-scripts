9b833d9772d7739d319b9c3422f2cf229b1ab8cc
Yiwei Gong <imwithye@gmail.com>
2025-09-03 19:58:28 +0800

    update kasm

--- kasm.sh
+++ kasm.sh
@@ -6,11 +6,14 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $SCRIPT_DIR/deps.sh
 
 if ! checkinstall vncserver; then
-    DEBIAN_FRONTEND=noninteractive apt install -qq -y dbus-x11 xfce4 xfce4-terminal
+    DEBIAN_FRONTEND=noninteractive apt-get install -qq -y dbus-x11 xfce4 xfce4-terminal
     rm -f /tmp/libssl1.1.deb
-    wget -O /tmp/libssl1.1.deb http://ftp.de.debian.org/debian/pool/main/o/openssl/libssl1.1_1.1.1w-0+deb11u1_amd64.deb
-    DEBIAN_FRONTEND=noninteractive apt install -qq -y /tmp/libssl1.1.deb
+    wget -q -O /tmp/libssl1.1.deb http://ftp.de.debian.org/debian/pool/main/o/openssl/libssl1.1_1.1.1w-0+deb11u1_amd64.deb
+    DEBIAN_FRONTEND=noninteractive apt-get install -qq -y /tmp/libssl1.1.deb
     rm -f /tmp/kasm.deb
-    wget -O /tmp/kasm.deb https://github.com/kasmtech/KasmVNC/releases/download/v1.3.4/kasmvncserver_bullseye_1.3.4_amd64.deb
-    DEBIAN_FRONTEND=noninteractive apt install -qq -y /tmp/kasm.deb
+    wget -q -O /tmp/kasm.deb https://github.com/kasmtech/KasmVNC/releases/download/v1.3.4/kasmvncserver_bullseye_1.3.4_amd64.deb
+    DEBIAN_FRONTEND=noninteractive apt-get install -qq -y /tmp/kasm.deb
 fi
+
+echo "Run `vncserver` to start the VNC server."
+echo "Run `pm2 start \"cloudflared tunnel --url https://172.28.0.12:8444 --no-tls-verify\"` to access the VNC server."
