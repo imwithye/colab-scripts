51393785037ad9ae0dfd7f9d3ed45d36787194d5
Yiwei Gong <imwithye@gmail.com>
2025-09-03 19:35:28 +0800

    install cloudflared

--- deps.sh
+++ deps.sh
@@ -14,14 +14,12 @@ checkfolder() {
     test -d $@ 2>&1 >/dev/null
 }
 
+if ! checkinstall cloudflared; then
+    rm -f /tmp/cloudflared.deb
+    wget -O /tmp/cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
+    DEBIAN_FRONTEND=noninteractive apt install -qq -y /tmp/cloudflared.deb
+fi
+
 if ! checkinstall pm2; then
     npm install -g pm2
-fi
-
-if ! checkpipinstall lightning; then
-    pip install lightning
-fi
-
-if ! checkpipinstall stable-baselines3; then
-    pip install stable-baselines3
 fi
